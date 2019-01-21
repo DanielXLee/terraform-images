f0ad8a16e1ae884224c4db28afff2853072a8952
Danielxlee <lixinxa@cn.ibm.com>
2019-01-21 20:53:17 +0800

    Update README

--- plugins/README.md
+++ plugins/README.md
@@ -1,3 +1,8 @@
 # Build Terraform Plugin
 
+The fyre plugin is a IBM intenal plugin, for to build it, need export 2 environments:
+```
+export IBM_GITHUB_USER=
+export IBM_GITHUB_TOKEN=
+```
 Run command `./build-plugin.sh`
--- plugins/build-plugin.sh
+++ plugins/build-plugin.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 
 # Build Terraform Plugin
-## Build Fyre Plugin
 [[ -d working_dir ]] && rm -rf working_dir/* || mkdir working_dir
 pushd working_dir
 mkdir linux_amd64 linux_ppc64le linux_s390x
+## Build Fyre Plugin
+IBM_GITHUB_USER=${IBM_GITHUB_USER:-}
+IBM_GITHUB_TOKEN=${IBM_GITHUB_TOKEN:-}
 #1. Build fyre plugin binary
-git clone https://lixinxa:878d7994b25bd68c4ff298fde7b07da7dcb209e1@github.ibm.com/bhwarren/terraform-fyre.git
-pushd terraform-fyre
-git checkout v1.1.1
-for arch in amd64 ppc64le s390x; do
-  docker run --rm -v $(pwd):/go/src/terraform-fyre \
-  -w /go/src/terraform-fyre \
-  -e GOOS=linux -e GOARCH=${arch} golang:latest bash -c "\
-  apt-get update && apt-get install -y zip && \
-  go get github.com/tmc/scp && \
-  go get github.com/spf13/cobra && \
-  go get -u golang.org/x/crypto/... && \
-  go get github.com/hashicorp/terraform && \
-  go build -o terraform-provider-fyre_${arch}"
-  cp terraform-provider-fyre_${arch} ../linux_${arch}/terraform-provider-fyre
-done
-popd
+if [[ "X${IBM_GITHUB_USER}" != "X" && "X${IBM_GITHUB_TOKEN}" != "X" ]]; then
+  git clone https://${IBM_GITHUB_USER}:${IBM_GITHUB_TOKEN}@github.ibm.com/bhwarren/terraform-fyre.git
+  pushd terraform-fyre
+  git checkout v1.1.1
+  for arch in amd64 ppc64le s390x; do
+    docker run --rm -v $(pwd):/go/src/terraform-fyre \
+    -w /go/src/terraform-fyre \
+    -e GOOS=linux -e GOARCH=${arch} golang:latest bash -c "\
+    apt-get update && apt-get install -y zip && \
+    go get github.com/tmc/scp && \
+    go get github.com/spf13/cobra && \
+    go get -u golang.org/x/crypto/... && \
+    go get github.com/hashicorp/terraform && \
+    go build -o terraform-provider-fyre_${arch}"
+    cp terraform-provider-fyre_${arch} ../linux_${arch}/terraform-provider-fyre
+  done
+  popd
+fi
+
 
 ## Build other build-in plugins
 for plugin in null template random tls local openstack vsphere aws; do
